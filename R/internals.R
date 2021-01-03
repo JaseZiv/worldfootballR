cdc8d91a7156ec47552b673a3f6da45351db8ef6
JaseZiv <jaseziv83@gmail.com>
2021-01-03 20:08:03 +1100

    cleaning function for advanced stats table

--- R/internals.R
+++ R/internals.R
@@ -71,3 +71,49 @@
   return(select_season)
 
 }
+
+
+
+
+#' Clean advanced statistic tables
+#'
+#' Returns cleaned dataframe for each of the team statistic tables
+#'
+#' @param advanced_stat_url url of the html table on the league season page
+#'
+#' @return a data frame for the selected league seasons advanced statistic
+#'
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#'
+.clean_advanced_stat_table <- function(advanced_stat_url) {
+
+  stat_df <- xml2::read_html(advanced_stat_url) %>%
+    rvest::html_table() %>%
+    data.frame()
+
+  var_names <- stat_df[1,] %>% as.character()
+
+  new_names <- paste(var_names, names(stat_df), sep = "_")
+
+  new_names <- new_names %>%
+    gsub("\\..*", "", .) %>%
+    gsub("_Var", "", .) %>%
+    gsub("# Pl", "Num_Players", .) %>%
+    gsub("%", "_percent", .) %>%
+    gsub("_Performance", "", .) %>%
+    gsub("_Penalty", "", .)
+
+  names(stat_df) <- new_names
+  stat_df <- stat_df[-1,]
+
+  cols_to_transform <- stat_df %>%
+    dplyr::select(-.data$Squad) %>% names()
+
+  stat_df <- stat_df %>%
+    dplyr::mutate_at(.vars = cols_to_transform, .funs = function(x) {gsub(",", "", x)}) %>%
+    dplyr::mutate_at(.vars = cols_to_transform, .funs = function(x) {gsub("+", "", x)}) %>%
+    dplyr::mutate_at(.vars = cols_to_transform, .funs = as.numeric)
+
+  return(stat_df)
+}
