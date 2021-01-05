bcced99e6b4ac8d58ac9f82b052a4ed528ea1461
JaseZiv <jaseziv83@gmail.com>
2021-01-05 21:53:14 +1100

    added match report and lineups function

---
+++ R/get_match_lineups.R
@@ -0,0 +1,61 @@
+#' Get match lineups
+#'
+#' Returns lineups for home and away teams for a selected match
+#'
+#' @param match_url the fbref.com URL for the required match
+#'
+#' @return returns a dataframe with the team lineups for a selected match
+#'
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' match <- "https://fbref.com/en/matches/47880eb7/Liverpool-Manchester-City-November-10-2019-Premier-League"
+#' get_match_lineups(match_url = match)
+#' }
+
+get_match_lineups <- function(match_url) {
+  match_page <- xml2::read_html(match_url)
+
+  game <- match_page %>% rvest::html_nodes("h1") %>% rvest::html_text()
+
+  print(glue::glue("Scraping lineups for {game}"))
+
+  lineups <- match_page %>% rvest::html_nodes(".lineup") %>% rvest::html_nodes("table")
+
+  home <- 1
+  away <- 2
+
+  get_each_lineup <- function(home_away) {
+    lineup <- lineups[home_away] %>% rvest::html_table() %>% data.frame()
+    formation <- names(lineup)[1] %>% gsub(".*\\.\\.", "", .) %>% stringr::str_extract_all(., "[[:digit:]]") %>% unlist() %>% paste(collapse = "-")
+    tryCatch( {team <- match_page %>% rvest::html_nodes("div:nth-child(1) div strong a") %>% rvest::html_text() %>% .[home_away]}, error = function(e) {team <- NA})
+
+    bench_index <- which(lineup[,1] == "Bench")
+
+    lineup <- lineup[1:(bench_index-1),] %>% dplyr::mutate(Starting = "Pitch") %>%
+      dplyr::bind_rows(
+        lineup[(bench_index+1):nrow(lineup),] %>% dplyr::mutate(Starting = "Bench")
+      )
+
+    lineup <- lineup %>%
+      dplyr::mutate(Matchday = game,
+             Team = team,
+             Formation = formation)
+
+    names(lineup) <- c("Player_Num", "Player_Name", "Starting", "Matchday", "Team", "Formation")
+
+    lineup <- lineup %>%
+      dplyr::select(.data$Matchday, .data$Team, .data$Formation, .data$Player_Num, .data$Player_Name, .data$Starting)
+
+    return(lineup)
+  }
+
+  all_lineup <- c(home, away) %>%
+    purrr::map_df(get_each_lineup)
+
+  return(all_lineup)
+}
---
+++ R/get_match_report.R
@@ -0,0 +1,60 @@
+#' Get match report
+#'
+#' Returns match report details for a selected match
+#'
+#' @param match_url the fbref.com URL for the required match
+#'
+#' @return returns a dataframe with the match details for a selected match
+#'
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' match <- "https://fbref.com/en/matches/47880eb7/Liverpool-Manchester-City-November-10-2019-Premier-League"
+#' get_match_report(match_url = match)
+#' }
+
+get_match_report <- function(match_url) {
+
+  Game_URL <- match_url
+  each_game_page <- NA
+  tryCatch( {each_game_page <- xml2::read_html(match_url)}, error = function(e) {each_game_page <- NA})
+
+  if(is.na(each_game_page)) stop("Invalid matchday URL")
+
+  game <- each_game_page %>% rvest::html_nodes("h1") %>% rvest::html_text()
+
+  print(glue::glue("Scraping {game}"))
+
+  tryCatch( {League <- each_game_page %>% rvest::html_nodes("h1+ div a") %>% rvest::html_text()}, error = function(e) {League <- NA})
+  tryCatch( {Match_Date <- each_game_page %>% rvest::html_nodes("div:nth-child(1) div strong a") %>% rvest::html_text() %>% .[3]}, error = function(e) {Match_Date <- NA})
+  tryCatch( {Matchweek <- each_game_page %>% rvest::html_nodes("h1+ div") %>% rvest::html_text()}, error = function(e) {Matchweek <- NA})
+
+  tryCatch( {Home_Team <- each_game_page %>% rvest::html_nodes("div:nth-child(1) div strong a") %>% rvest::html_text() %>% .[1]}, error = function(e) {Home_Team <- NA})
+  tryCatch( {Home_Formation <- each_game_page %>% rvest::html_nodes(".lineup#a") %>% rvest::html_nodes("th") %>% rvest::html_text() %>% .[1] %>% gsub(".*\\(", "", .) %>% gsub("\\)", "", .)}, error = function(e) {Home_Formation <- NA})
+  tryCatch( {Home_Score <- each_game_page %>% rvest::html_nodes(".scores") %>% rvest::html_nodes(".score") %>% rvest::html_text() %>% .[1]}, error = function(e) {Home_Score <- NA})
+  tryCatch( {Home_xG <- each_game_page %>% rvest::html_nodes(".scores") %>% rvest::html_nodes(".score_xg") %>% rvest::html_text() %>% .[1]}, error = function(e) {Home_xG <- NA})
+  tryCatch( {Home_Goals <- each_game_page %>% rvest::html_nodes("#a") %>% .[[1]] %>% rvest::html_text()}, error = function(e) {Home_Goals <- NA}) %>%
+    stringr::str_remove_all("\n") %>% stringr::str_remove_all("\t")
+
+  tryCatch( {Away_Team <- each_game_page %>% rvest::html_nodes("div:nth-child(1) div strong a") %>% rvest::html_text() %>% .[2]}, error = function(e) {Away_Team <- NA})
+  tryCatch( {Away_Formation <- each_game_page %>% rvest::html_nodes(".lineup#b") %>% rvest::html_nodes("th") %>% rvest::html_text() %>% .[1] %>% gsub(".*\\(", "", .) %>% gsub("\\)", "", .)}, error = function(e) {Away_Formation <- NA})
+  tryCatch( {Away_Score <- each_game_page %>% rvest::html_nodes(".scores") %>% rvest::html_nodes(".score") %>%rvest:: html_text() %>% .[2]}, error = function(e) {Away_Score <- NA})
+  tryCatch( {Away_xG <- each_game_page %>% rvest::html_nodes(".scores") %>% rvest::html_nodes(".score_xg") %>% rvest::html_text() %>% .[2]}, error = function(e) {Away_xG <- NA})
+  tryCatch( {Away_Goals <- each_game_page %>% rvest::html_nodes("#b") %>% .[[1]] %>% rvest::html_text()}, error = function(e) {Away_Goals <- NA}) %>%
+    stringr::str_remove_all("\n") %>% stringr::str_remove_all("\t")
+
+
+  each_game <- cbind(League, Match_Date, Matchweek, Home_Team, Home_Formation, Home_Score, Home_xG, Home_Goals, Away_Team, Away_Formation, Away_Score, Away_xG, Away_Goals, Game_URL) %>%
+    dplyr::as_tibble() %>%
+    dplyr::mutate(Home_Score = as.numeric(.data$Home_Score),
+                  Home_xG = as.numeric(.data$Home_xG),
+                  Away_Score = as.numeric(.data$Away_Score),
+                  Away_xG = as.numeric(.data$Away_xG))
+
+  return(each_game)
+
+}
