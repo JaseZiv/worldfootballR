31cb5a7e89dca401c10412630f50f5be0e24e53d
JaseZiv <jaseziv83@gmail.com>
2021-01-06 14:30:22 +1100

    vectorised get_match_results

--- R/get_match_results.R
+++ R/get_match_results.R
@@ -1,74 +1,101 @@
 #' Get match results
 #'
-#' Returns the game results for a given league season
+#' Returns the game results for a given league season(s)
 #'
 #' @param country the three character country code
 #' @param gender gender of competition, either "M" or "F"
-#' @param season_end_year the year the season concludes, in quotes, ie "2021"
+#' @param season_end_year the year(s) the season concludes
 #'
 #' @return returns a dataframe with the results of the competition, season and gender
 #'
 #' @importFrom magrittr %>%
 #' @importFrom rlang .data
+#' @importFrom utils read.csv
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' get_match_results(country = "ITA", gender = "M", season_end_year = "2020")
+#' get_match_results(country = c("ENG", "ITA", "GER"), gender = "M", season_end_year = c(2018:2021))
 #' }
-
 
 get_match_results <- function(country, gender, season_end_year) {
 
-  print(paste0("Scraping ", country, " fist division for the ", season_end_year, " season (gender = ", gender, ")"))
+  print("Scraping match results")
 
-  competitions <- .get_tier1_competitions()
+  country_abbr <- country
+  gender_M_F <- gender
+  season_end_year_num <- season_end_year
+
 
   main_url <- "https://fbref.com"
 
-  country <- country
-  gender <- gender
-  season <- season_end_year
-  select_season <- .get_league_season_url(country, gender, season)
+  seasons <- read.csv("https://raw.githubusercontent.com/JaseZiv/worldfootballR_data/master/raw-data/league_seasons/all_tier1_season_URLs.csv")
+
+  seasons <- seasons %>%
+    dplyr::filter(country %in% country_abbr,
+                  gender %in% gender_M_F,
+                  season_end_year %in% season_end_year_num)
+
+  seasons_urls <- seasons %>%
+    dplyr::pull(seasons_urls)
 
 
-  fixtures_url <- xml2::read_html(select_season) %>%
-    rvest::html_nodes(".hoversmooth") %>%
-    rvest::html_nodes(".full") %>%
-    rvest::html_nodes("a") %>%
-    rvest::html_attr("href") %>% .[grepl("Fixtures", .)] %>% paste0(main_url, .)
+  get_each_season_results <- function(season_url) {
+
+    fixtures_url <- xml2::read_html(season_url) %>%
+      rvest::html_nodes(".hoversmooth") %>%
+      rvest::html_nodes(".full") %>%
+      rvest::html_nodes("a") %>%
+      rvest::html_attr("href") %>% .[grepl("Fixtures", .)] %>% paste0(main_url, .)
+
+    fixtures_page <- xml2::read_html(fixtures_url)
+
+    season_name <- fixtures_page %>% rvest::html_nodes("h2 span") %>% rvest::html_text() %>% .[1]
+
+    season_summary <- fixtures_page %>%
+      rvest::html_table() %>% .[1] %>% data.frame() %>%
+      dplyr::filter(.data$Date != "")
 
 
-  season_summary <- xml2::read_html(fixtures_url) %>%
-    rvest::html_table() %>% .[1] %>% data.frame() %>%
-    dplyr::filter(.data$Date != "")
+    suppressWarnings(
+      season_summary <- season_summary %>%
+        tidyr::separate(.data$Score, into = c("HomeGoals", "AwayGoals"), sep = "–") %>%
+        dplyr::mutate(HomeGoals = as.numeric(.data$HomeGoals),
+                      AwayGoals = as.numeric(.data$AwayGoals),
+                      Attendance = as.numeric(gsub(",", "", .data$Attendance)))
+    )
+
+    season_summary <- cbind(season_url, season_name, season_summary)
+
+    if(!any(stringr::str_detect(names(season_summary), "Round"))) {
+      Round <- rep(NA, nrow(season_summary))
+      season_summary <- cbind(Round, season_summary)
+    }
+
+    if(any(stringr::str_detect(names(season_summary), "xG"))) {
+      season_summary <- season_summary %>%
+        dplyr::select(.data$season_url, Season_Name=.data$season_name, Round, .data$Wk, .data$Day, .data$Date, .data$Time, .data$Home, .data$HomeGoals, Home_xG=.data$xG, .data$Away, .data$AwayGoals, Away_xG=.data$xG.1, .data$Attendance, .data$Venue, .data$Referee, .data$Notes)
+    } else {
+      season_summary <- season_summary %>%
+        dplyr::select(.data$season_url, Season_Name=.data$season_name, Round, .data$Wk, .data$Day, .data$Date, .data$Time, .data$Home, .data$HomeGoals, .data$Away, .data$AwayGoals, .data$Attendance, .data$Venue, .data$Referee, .data$Notes)
+    }
 
 
-  suppressWarnings(
-    season_summary <- season_summary %>%
-      tidyr::separate(.data$Score, into = c("HomeGoals", "AwayGoals"), sep = "–") %>%
-      dplyr::mutate(HomeGoals = as.numeric(.data$HomeGoals),
-             AwayGoals = as.numeric(.data$AwayGoals),
-             Attendance = as.numeric(gsub(",", "", .data$Attendance)))
-  )
-
-  season_summary <- cbind(season_end_year, season_summary)
-
-  if(!any(stringr::str_detect(names(season_summary), "Round"))) {
-    Round <- rep(NA, nrow(season_summary))
-    season_summary <- cbind(Round, season_summary)
+    return(season_summary)
   }
 
-  if(any(stringr::str_detect(names(season_summary), "xG"))) {
-    season_summary <- season_summary %>%
-      dplyr::select(Season=season_end_year, Round, .data$Wk, .data$Day, .data$Date, .data$Time, .data$Home, .data$HomeGoals, Home_xG=.data$xG, .data$Away, .data$AwayGoals, Away_xG=.data$xG.1, .data$Attendance, .data$Venue, .data$Referee, .data$Notes)
-  } else {
-    season_summary <- season_summary %>%
-      dplyr::select(Season=season_end_year, Round, .data$Wk, .data$Day, .data$Date, .data$Time, .data$Home, .data$HomeGoals, .data$Away, .data$AwayGoals, .data$Attendance, .data$Venue, .data$Referee, .data$Notes)
-  }
+  all_results <- seasons_urls %>%
+    purrr::map_df(get_each_season_results)
 
+  all_results <- seasons %>%
+    dplyr::select(Competition_Name=.data$competition_name, Gender=.data$gender, Country=.data$country, Season_End_Year=.data$season_end_year, .data$seasons_urls) %>%
+    dplyr::left_join(all_results, by = c("seasons_urls" = "season_url")) %>%
+    dplyr::select(-.data$seasons_urls) %>%
+    dplyr::mutate(Date = lubridate::ymd(.data$Date)) %>%
+    dplyr::arrange(.data$Country, .data$Competition_Name, .data$Gender, .data$Season_End_Year, .data$Wk, .data$Date, .data$Time)
 
-  return(season_summary)
+  print("Match results finished scraping")
 
+  return(all_results)
 }
