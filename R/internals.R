e6a8b54c50ca4bc4214f7b4cf19dd1f38f60c955
Tony ElHabr <anthonyelhabr@gmail.com>
2022-08-20 08:20:32 -0500

    try loading as option

--- R/internals.R
+++ R/internals.R
@@ -380,12 +380,6 @@
 #   return(NULL)
 # }
 
-.upper1 <- function(x) {
-  x <- tolower(x)
-  substr(x, 1, 1) <- toupper(substr(x, 1, 1))
-  x
-}
-
 #' Load Page with headers
 #'
 #' loads webpages with a header passed to read_html
@@ -394,32 +388,11 @@
 #'
 #' @return a html webpage
 #'
-#' @importFrom magrittr %>%
 #' @noRd
 #'
 .load_page <- function(page_url) {
-  prefix <- if(interactive() & rstudioapi::isAvailable()) {
-    version_info <- rstudioapi::versionInfo()
-    sprintf(
-      "RStudio %s (%s)",
-      .upper1(version_info$mode),
-      version_info$version
-    )
-  } else {
-    "RStudio Desktop (2022.7.1.554)"
-  }
-  session_info <- utils::sessionInfo()
-  r_version <- session_info$R.version
-  agent <- sprintf(
-    "%s; R (%s.%s %s %s %s)",
-    prefix,
-    r_version$major,
-    r_version$minor,
-    r_version$platform,
-    r_version$arch,
-    r_version$arch
-  )
-
+  agent <- getOption("worldfootballR.agent", default = "RStudio Desktop (2022.7.1.554); R (4.1.1 x86_64-w64-mingw32 x86_64 mingw32)")
   ua <- httr::user_agent(agent)
-  rvest::session(url = page_url, ua) %>% xml2::read_html()
+  session <- rvest::session(url = page_url, ua)
+  xml2::read_html(session)
 }
