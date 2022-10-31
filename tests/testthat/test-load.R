bd27e6efcfda0713ac25ca4bcc35ad1a973cb764
Tony ElHabr <anthonyelhabr@gmail.com>
2022-10-31 08:03:20 -0500

    update fotmob load tests

--- tests/testthat/test-load.R
+++ tests/testthat/test-load.R
@@ -74,16 +74,17 @@ test_that("load_fotmob_matches_by_date() works", {
   testthat::skip_on_cran()
   testthat::skip_if_offline()
 
-  n_cols_match_details <- 41
+  expected_matches_by_date_cols <- c("away_id", "away_name", "away_score", "ccode", "home_id", "home_name", "home_score", "id", "match_id", "match_league_id")
   epl_matches <- load_fotmob_matches_by_date(league_id = 47)
   expect_type(epl_matches, "list")
   n_rows_epl_matches <- nrow(epl_matches)
   expect_gt(n_rows_epl_matches, 0)
-  expect_equal(ncol(epl_matches), n_cols_match_details)
+  ## There have been issues where the columns are not in the same exact order depending on the day, so rely on sort
+  expect_true(all(expected_matches_by_date_cols %in% colnames(epl_matches)))
 
   epl_ll_matches <- load_fotmob_matches_by_date(league_id = c(47, 87))
   expect_gt(nrow(epl_ll_matches), n_rows_epl_matches)
-  expect_equal(ncol(epl_matches), n_cols_match_details)
+  expect_true(all(expected_matches_by_date_cols %in% colnames(epl_ll_matches)))
 
   expect_error(load_fotmob_matches_by_date(league_id = 0))
 })
@@ -92,16 +93,17 @@ test_that("load_fotmob_match_details() works", {
   testthat::skip_on_cran()
   testthat::skip_if_offline()
 
-  n_cols_match_details <- 42
+  ## "short_name" appears for a very small set of rows
+  expected_match_detail_cols <- c("match_id", "match_round", "league_id", "league_name", "league_round_name", "parent_league_id", "parent_league_season", "match_time_utc", "home_team_id", "home_team", "home_team_color", "away_team_id", "away_team", "away_team_color", "id", "event_type", "team_id", "player_id", "player_name", "x", "y", "min", "min_added", "is_blocked", "is_on_target", "blocked_x", "blocked_y", "goal_crossed_y", "goal_crossed_z", "expected_goals", "expected_goals_on_target", "shot_type", "situation", "period", "is_own_goal", "on_goal_shot_x", "on_goal_shot_y", "on_goal_shot_zoom_ratio", "first_name", "last_name", "team_color", "short_name")
   epl_match_details <- load_fotmob_match_details(league_id = 47)
   expect_type(epl_match_details, "list")
   n_rows_epl_match_details <- nrow(epl_match_details)
   expect_gt(n_rows_epl_match_details, 0)
-  expect_equal(ncol(epl_match_details), n_cols_match_details)
+  expect_equal(sort(colnames(epl_match_details)), sort(expected_match_detail_cols))
 
   epl_ll_match_details <- load_fotmob_match_details(league_id = c(47, 87))
   expect_gt(nrow(epl_ll_match_details), n_rows_epl_match_details)
-  expect_equal(ncol(epl_ll_match_details), n_cols_match_details)
+  expect_equal(sort(colnames(epl_ll_match_details)), sort(expected_match_detail_cols))
 
   expect_error(load_fotmob_match_details(league_id = 0))
 })
