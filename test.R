099eb3d7396516dfd4404ff2b17f02f0c935de1b
Tony ElHabr <anthonyelhabr@gmail.com>
2023-06-03 07:13:13 -0500

    install devtools first

--- test.R
+++ test.R
@@ -1,3 +1,4 @@
+devtools::load_all()
 league_one_injuries <- tm_league_injuries(country_name = "",league_url = "https://www.transfermarkt.com/league-one/startseite/wettbewerb/GB3")
 hazard_injuries <- tm_player_injury_history(player_urls = "https://www.transfermarkt.com/eden-hazard/profil/spieler/50202")
 
