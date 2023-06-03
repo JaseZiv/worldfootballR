cd293ed9fd409c6f60006683fd120e1b31d842e4
Tony ElHabr <anthonyelhabr@gmail.com>
2023-06-03 08:33:32 -0500

    try f

--- test.R
+++ test.R
@@ -1,6 +1,16 @@
 library(worldfootballR)
+library(purrr)
 packageVersion("worldfootballR")
-league_one_injuries <- tm_league_injuries(country_name = "",league_url = "https://www.transfermarkt.com/league-one/startseite/wettbewerb/GB3")
-league_one_injuries
-hazard_injuries <- tm_player_injury_history(player_urls = "https://www.transfermarkt.com/eden-hazard/profil/spieler/50202")
-hazard_injuries
+
+try_f <- function(f, ...) {
+  fs <- safely(f, quiet = FALSE)
+  res <- fs(...)
+  if (!is.null(res$error)) {
+    print(res$error)
+  }
+  res$result
+}
+
+try_f(tm_matchday_table, country_name="England", start_year="2021", matchday=1)
+try_f(tm_league_injuries, country_name = "",league_url = "https://www.transfermarkt.com/league-one/startseite/wettbewerb/GB3")
+try_f(tm_player_injury_history, player_urls = "https://www.transfermarkt.com/eden-hazard/profil/spieler/50202")
