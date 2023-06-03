2ea81b5fbfc98275c944e9d4955494c1292c7c94
Tony ElHabr <anthonyelhabr@gmail.com>
2023-06-03 06:55:03 -0500

    dispatch on branch

--- tests/test.R
+++ tests/test.R
@@ -1,4 +1,3 @@
 league_one_injuries <- tm_league_injuries(country_name = "",league_url = "https://www.transfermarkt.com/league-one/startseite/wettbewerb/GB3")
-
 hazard_injuries <- tm_player_injury_history(player_urls = "https://www.transfermarkt.com/eden-hazard/profil/spieler/50202")
 
