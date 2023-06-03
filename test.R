f0fa69b15a0d6a621d0379c5214039cfa65deb39
Tony ElHabr <anthonyelhabr@gmail.com>
2023-06-03 08:24:01 -0500

    install worldfootballR

--- test.R
+++ test.R
@@ -1,4 +1,6 @@
-devtools::load_all()
+library(worldfootballR)
+packageVersion("worldfootballR")
 league_one_injuries <- tm_league_injuries(country_name = "",league_url = "https://www.transfermarkt.com/league-one/startseite/wettbewerb/GB3")
+league_one_injuries
 hazard_injuries <- tm_player_injury_history(player_urls = "https://www.transfermarkt.com/eden-hazard/profil/spieler/50202")
-
+hazard_injuries
