cfd66dd528887d18446ef4a34a8a35fc965c68d6
ForgeGit <jruizcabrejos@gmail.com>
2024-07-06 14:51:44 +0200

    Update understat_match_players.R

--- R/understat_match_players.R
+++ R/understat_match_players.R
@@ -31,7 +31,14 @@ match_player_data <- match_player_data[grep("rostersData\t=", match_player_data)
   stringr::str_subset("\\[\\]", negate = TRUE)
 
 match_player_data <- lapply(match_player_data, jsonlite::fromJSON) %>%
-  do.call("rbind", .) %>%
+  do.call("rbind", .)
+
+match_player_data_home <- do.call(rbind.data.frame, match_player_data$h)
+match_player_data_away <- do.call(rbind.data.frame, match_player_data$a)
+
+match_player_data <- bind_rows(match_player_data_home,match_player_data_away) %>%
+  mutate(match_id = match_id)  %>%
+
   select(match_id,  team_id,
          team_status = h_a,
          player_id, swap_id = id,
@@ -39,13 +46,6 @@ match_player_data <- lapply(match_player_data, jsonlite::fromJSON) %>%
          time_played = time,
          everything()) %>%
   mutate(team_status = ifelse(team_status=="h","home","away"))
-
-
-match_player_data_home <- do.call(rbind.data.frame, match_player_data$h)
-match_player_data_away <- do.call(rbind.data.frame, match_player_data$a)
-
-match_player_data <- bind_rows(match_player_data_home,match_player_data_away) %>%
-  mutate(match_id = match_id)
 
   return(match_player_data)
 }
